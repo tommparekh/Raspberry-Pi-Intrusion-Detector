7f81537f3be58b3bed7ef988bb37cdcde7f265cf
tommparekh <mihir.m.parekh@gmail.com>
2019-10-26 17:30:19 -0400

    Add files via upload

--- parent-detector.py
+++ parent-detector.py
@@ -1,6 +1,7 @@
 from gpiozero import MotionSensor
-from picamera import PiCamera
+from picamera import PiCamera, Color
 from datetime import datetime
+from time import sleep
 
 print(datetime.now());
 
@@ -12,21 +13,27 @@ record_file_path = "/home/pi/Desktop/"
 record_file = record_file_path + timestamp + "-intruder.h264";
 
 
-while True:
-    pir.wait_for_motion()
-    print("YEYY...Motion Detected...")
-
-    camera.start_recording(record_file)    
-        
-    pir.wait_for_no_motion()
-    print("---No Motion Detected---")
-    camera.stop_recording()
-
-
-
-
-
-def start_camera_pic_action():
-    camera.capture('/home/pi/Desktop/campic.png')
+def start_camera_preview():
+#    camera.start_preview(alpha=100)
     camera.start_preview()
+#    camera.brightness = 50
+#    camera.contrast=50    
+    camera.annotate_background = Color('black')
+    camera.annotate_foreground = Color('white')
+    camera.annotate_text = " Recording in progress "
+    sleep(5)
     camera.stop_preview()
+
+def intruder_video_capture():
+    while True:
+        pir.wait_for_motion()
+        print("YEYY...Motion Detected...")
+        camera.start_recording(record_file)              
+        pir.wait_for_no_motion()
+        print("---No Motion Detected---")
+        camera.stop_recording()
+
+def start_camera_pic_capture():
+    camera.capture('/home/pi/Desktop/campic.png')
+
+start_camera_preview()
